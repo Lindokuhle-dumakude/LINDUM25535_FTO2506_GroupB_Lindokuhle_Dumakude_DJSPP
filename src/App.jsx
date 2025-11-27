85597bb772e7086a173b56412c752fed760d2de6
unknown <lindokuhle.dumakude11@gmail.com>
2025-11-27 08:39:18 +0200

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import ShowDetail from "./pages/ShowDetail";
+import Favourites from "./pages/Favourites";
+import { PodcastProvider } from "./context/PodcastContext";
+import { FavouritesProvider } from "./favourites/FavouritesContext";
+import { AudioPlayerProvider } from "./audio/AudioPlayerContext";
+import AudioPlayer from "./audio/AudioPlayer";
+import { ThemeProvider } from "./theme/ThemeContext";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/show/:id" element={<ShowDetail />} />
-    </Routes>
+    <ThemeProvider>
+      <PodcastProvider>
+        <FavouritesProvider>
+          <AudioPlayerProvider>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/show/:id" element={<ShowDetail />} />
+              <Route path="/favourites" element={<Favourites />} />
+            </Routes>
+
+            <AudioPlayer />
+          </AudioPlayerProvider>
+        </FavouritesProvider>
+      </PodcastProvider>
+    </ThemeProvider>
   );
 }
